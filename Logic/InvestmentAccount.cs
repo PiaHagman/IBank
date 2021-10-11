5f7100d6959426233da70aa196fe9d3291ac9027
Pia Hagman <pia.hagman@utb.ecutbildning.se>
2021-10-11 10:13:09 +0200

    [Green] InvestmentAccount_Tests(), issue #14

--- Logic/InvestmentAccount.cs
+++ Logic/InvestmentAccount.cs
@@ -16,6 +16,30 @@ namespace Logic
 
         public override bool WithdrawFunds(double withdrawal, IDate date)
         {
+            double totalWithdrawalsPerYear = 0;
+
+            if ((GetBalance() - withdrawal) < 0)
+            {
+                return false;
+            }
+
+            foreach (var transaction in _investmentAccountTransactions)
+            {
+                if (totalWithdrawalsPerYear>=1 && transaction.Date.Year == date.Year() && transaction.Amount < 0)
+                {
+                    return false;
+                }
+
+                if (transaction.Date.Year != date.Year())
+                {
+                    totalWithdrawalsPerYear = 0;
+                }
+
+                totalWithdrawalsPerYear++;
+            }
+
+            var newWithDrawlWithBankCharges = new Transaction(-withdrawal, date.Today());
+            _investmentAccountTransactions.Add(newWithDrawlWithBankCharges);
             return true;
         }
     }
