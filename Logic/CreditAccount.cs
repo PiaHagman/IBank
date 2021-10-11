190f22c489dd30dcd67db02d8689ee8113b66cc4
Sofiia Löf <sofiia.lof@learnet.se>
2021-10-11 14:09:32 +0200

    [Green] CreditAccount_Tests() issue #15

--- Logic/CreditAccount.cs
+++ Logic/CreditAccount.cs
@@ -9,6 +9,8 @@ namespace Logic
    public class CreditAccount : Account
     {
         private readonly List<Transaction> _creditAccountTransactions;
+        private double _creditBalance= 20000;
+
         public CreditAccount(List<Transaction> allTransactions) : base(allTransactions)
         {
             _creditAccountTransactions = allTransactions;
@@ -16,7 +18,22 @@ namespace Logic
 
         public override bool WithdrawFunds(double withdrawal, IDate date)
         {
-            return false;
+            double currentBalance = GetBalance();
+
+            if (currentBalance < withdrawal)
+            {
+               _creditBalance -= (withdrawal - currentBalance);
+
+               if (_creditBalance < 0)
+               {
+                   return false;
+               }
+            }
+            
+            var newWithDrawlWithBankCharges = new Transaction(-withdrawal, date.Today());
+            _creditAccountTransactions.Add(newWithDrawlWithBankCharges);
+
+            return true;
         }
     }
 }
