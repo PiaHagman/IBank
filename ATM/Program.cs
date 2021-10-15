7dbf77274519b689b8cc8e4974d96d61ae2e02db
Pia Hagman <pia.hagman@utb.ecutbildning.se>
2021-10-15 09:20:44 +0200

    Startat ConsolApp för framtida utveckling

---
+++ ATM/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using Logic;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DebitAccount debitAccount = new DebitAccount(new List<Transaction>());
+            int choice;
+            RealDate date = new RealDate();
+
+            Console.WriteLine("***Välkommen till uttagsautomaten!***");
+            Console.WriteLine("Vänligen gör ditt val, 1-4:");
+            Console.WriteLine("1. Kolla saldo");
+            Console.WriteLine("2. Uttag");
+            Console.WriteLine("3. Insättning");
+            Console.WriteLine("4. Avsluta");
+
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine($"Ditt saldo är {debitAccount.GetBalance()} SEK");
+                    break;
+                case 2:
+                    Console.WriteLine("Ange belopp för uttag:");
+                    double withDrawal = double.Parse(Console.ReadLine());
+                    debitAccount.WithdrawFunds(withDrawal, date);
+            }
+
+        }
+    }
+}
